28c0f87e2dadc00dbeb2fc055d86f15de1fc00e7
fplaisant-ntico <florentin.plaisant@ntico.com>
2025-05-12 09:54:06 +0200

    add cd.yml

--- src/main/java/com/fplaisant/hrapi/model/Employee.java
+++ src/main/java/com/fplaisant/hrapi/model/Employee.java
@@ -30,43 +30,4 @@ public class Employee {
     @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
 }
